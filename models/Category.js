24dce92007fa30dfe076431637ea00f9620e607b
amjr86-DBxjRd <amjr86@outlook.com>
2022-07-05 23:29:46 -0700

    primary key id

--- models/Category.js
+++ models/Category.js
@@ -10,7 +10,13 @@ Category.init(
     category_name: {
       type: DataTypes.STRING,
       allowNull: false,
-    }
+    },
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
   },
   {
     sequelize,
