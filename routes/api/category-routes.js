bc6d49fb7f111c7939be0d41a1c645638261d236
amjr86-DBxjRd <amjr86@outlook.com>
2022-07-06 21:04:54 -0700

    category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -29,11 +29,19 @@ router.get('/:id', async (req, res) => {
     }
 
     res.status(200).json(categoryData);
+  }catch (err) {
+    res.status(500).json(err);
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try{
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
@@ -42,6 +50,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!categoryData) {
+      res.status(400).json({ message: "No category found with this id!"});
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
